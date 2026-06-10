dacf69b583e15460d44db9f47f6375b6332f6f04
0xJsantos <santosjohnbless@gmail.com>
2026-06-09 23:34:46 -0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,12 +11,12 @@ import ContactFooter from '../components/ContactFooter';
 import HowItWorks from '../components/HowItWorks';
 
 const IMAGES = {
-  hero: 'https://media.db.com/images/public/6a1b899011c8046fc7d701ef/e6ba02b4e_generated_1799b4e1.png',
-  landscape: 'https://media.db.com/images/public/6a1b899011c8046fc7d701ef/d393ed892_generated_17442dee.png',
-  healthcare: 'https://media.db.com/images/public/6a1b899011c8046fc7d701ef/2e39d95ae_generated_b970eb7b.png',
-  business: 'https://media.db.com/images/public/6a1b899011c8046fc7d701ef/fa5fa9fcc_generated_1061987b.png',
-  consultation: 'https://media.db.com/images/public/6a1b899011c8046fc7d701ef/97aad028f_generated_c5438b8c.png',
-  education: 'https://media.db.com/images/public/6a1b899011c8046fc7d701ef/be1920cf7_generated_aa5bd29a.png',
+  hero: '/images/graduate.jpg',
+  landscape: '/images/Strava_College.png',
+  healthcare: '/images/MTG_HealthCareAcademy.jpg',
+  business: '/images/ABM_College.png',
+  consultation: '/images/consultation.png',
+  education: '/images/RCB_College.png',
 };
 
 export default function Home() {
